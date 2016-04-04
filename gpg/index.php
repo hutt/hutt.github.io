f9b36277c53695959653b4546a2d506146162bfc
77u4 <github@jh0.eu>
2016-04-04 18:54:57 +0200

    removed invalid short url /gpg

--- gpg/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = "../pubkey.asc";  
-
-header('Content-Type: text/plain');  
-header('Content-Disposition: inline; filename= "'.$file.'"');  
-header('Content-Length: '.filesize($file));  
-
-readfile($file);
-?>
