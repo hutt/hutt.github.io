3b8005762a3f59bb01312c13fe62245e9742cd9a
hutt <jannis@hutt.io>
2016-06-02 13:17:56 +0200

    implemented own foursquare library

--- js/foursquare.js
+++ js/foursquare.js
@@ -1,8 +1,37 @@
-function getLastCheckinData()
-{
-	var url = 'https://api.jh0.eu/swarm/';
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+function loadUserData(config){
+	var data, url, xmlHttp;
+
+	//VARS from config
+	var callback = config.customCallback;
+
+	url = 'https://api.jh0.eu/swarm?fetch=user';
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function(data) {
+    	if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+    		var niceAnswer = JSON.parse(xmlHttp.responseText)[0];
+    		callback(niceAnswer);
+    	}
+  	}
+}
+
+function loadCheckins(config){
+	var data, url, xmlHttp;
+
+	//VARS from config
+	var count = config.count;
+	var callback = config.customCallback;
+
+	if(count > 0 && count < 50){
+		url = 'https://api.jh0.eu/swarm?fetch=checkins&count=' + count;
+	    xmlHttp = new XMLHttpRequest();
+	    xmlHttp.onreadystatechange = function(data) {
+	    	if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+	    		var niceAnswer = JSON.parse(xmlHttp.responseText)[0];
+	    		callback(niceAnswer);
+	    	}
+	  	}
+	}
+
+    xmlHttp.open( "GET", url, true );
+    xmlHttp.send();
 }
