625cc86c53b911ada3a943bf141799f93b310040
hutt <jannis@hutt.io>
2016-06-10 18:40:31 +0200

    trying to make cors work.

--- js/foursquare.js
+++ js/foursquare.js
@@ -6,13 +6,15 @@ function loadUserData(config){
 
 	url = 'https://api.jh0.eu/swarm?fetch=user';
     xmlHttp = new XMLHttpRequest();
+	xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+	xmlHttp.setRequestHeader('Access-Control-Allow-Methods', 'GET');
     xmlHttp.onreadystatechange = function(data) {
     	if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
 			
 			var answer = JSON.parse(xmlHttp.responseText)
     		var niceAnswer = answer[0];
     		callback(niceAnswer);
-    		
+
     	}
   	}
 
@@ -33,6 +35,8 @@ function loadCheckins(config){
 	if(count > 0 && count < 50){
 		url = 'https://api.jh0.eu/swarm?fetch=checkins&count=' + count;
 	    xmlHttp = new XMLHttpRequest();
+		xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+		xmlHttp.setRequestHeader('Access-Control-Allow-Methods', 'GET');
 	    xmlHttp.onreadystatechange = function(data) {
 	    	if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
 
