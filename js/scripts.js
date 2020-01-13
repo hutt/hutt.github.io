91534291c1910f933ef9ab93d4eb85fbe9128e5d
hutt <jannis@hutt.io>
2020-01-13 16:16:42 +0100

    fixed getAge()

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 loadCheckins({
-  "count" : 1,
-  "ignore": ["Home (private)", "Food", "Residence"],
-  "customCallback": showIntroText
+  count: 1,
+  ignore: ["Home (private)", "Food", "Residence"],
+  customCallback: showIntroText
 });
 
-function refreshIntro(){
+function refreshIntro() {
   loadCheckins({
-    "count" : 1,
-    "ignore": ["Home (private)", "Food", "Residence"],
-    "customCallback": showIntroText
+    count: 1,
+    ignore: ["Home (private)", "Food", "Residence"],
+    customCallback: showIntroText
   });
 }
 
 function getAge() {
-  var element = document.getElementById('age');
+  var element = document.getElementById("age");
   var today = new Date();
   var birthday = new Date(1997, 2, 3);
 
@@ -22,27 +22,43 @@ function getAge() {
   var m = today.getMonth() - birthday.getMonth();
 
   if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
-          jahr--;
+    jahr--;
   }
 
-  var monat = ((today.getMonth() - birthday.getMonth()) / 12).toPrecision(1)*10;
+  if (today.getMonth() < birthday.getMonth()) {
+    var monat =
+      ((12 + today.getMonth() - birthday.getMonth()) / 12).toPrecision(1) * 10;
+  } else {
+    var monat =
+      ((today.getMonth() - birthday.getMonth()) / 12).toPrecision(1) * 10;
+  }
 
-  return jahr + ',' + (monat);
+  return jahr + "," + monat;
 }
 
 function showIntroText(lastSeen) {
-  var element = document.getElementById('intro-text');
+  var element = document.getElementById("intro-text");
   var text;
 
-  var de = 'Hi, mein Name ist Jannis. '
-           + 'Ich bin ' + getAge() + ' Jahre alt und wurde zuletzt ' + lastSeen.de + ' gesehen.';
+  var de =
+    "Hi, mein Name ist Jannis. " +
+    "Ich bin " +
+    getAge() +
+    " Jahre alt und wurde zuletzt " +
+    lastSeen.de +
+    " gesehen.";
 
-  var en ='Hi, I\'m Jannis. '
-           + 'I\'m ' + getAge() + ' years old and I was seen ' + lastSeen.en + ' the last time.';
+  var en =
+    "Hi, I'm Jannis. " +
+    "I'm " +
+    getAge() +
+    " years old and I was seen " +
+    lastSeen.en +
+    " the last time.";
 
-  if(getCookie('lang') == "en"){
+  if (getCookie("lang") == "en") {
     text = en;
-  }else{
+  } else {
     text = de;
   }
 
