0ee8df4fd660f6406acee0c8bcb43c0d3c4f953d
hutt <jannis@hutt.io>
2022-08-02 23:24:36 +0200

    Version 2 based on paradigm shift theme

---
+++ assets/js/mailform.js
@@ -0,0 +1,83 @@
+/*
+	 _           _   _   
+	| |         | | | |  
+	| |__  _   _| |_| |_ 
+	| '_ \| | | | __| __|
+	| | | | |_| | |_| |_ 
+	|_| |_|\__,_|\__|\__|
+
+	Mail Form Skript
+
+*/
+
+const form = document.getElementById('kontaktformular');
+
+function resetForm () {
+	form.elements.name = "";
+	form.elements.email = "";
+	form.elements.category.value = "blank";
+	form.elements.copy.checked = true;
+	form.elements.human.checked = false;
+	form.elements.message = "";
+}
+
+function validateMailAddress (input) {
+	const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	if ( input.match(re) ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function validateForm () {
+	if (form.elements.human.checked == true && form.elements.name.value != "" && validateMailAddress(form.elements.email.value) != "" && form.elements.message.value != ""){
+		form.elements.submit.disabled = false;
+	} else {
+		form.elements.submit.disabled = true;
+	}
+}
+
+
+// Validation bei KeyUp
+form.addEventListener('keyup', function (event){
+	validateForm();
+});
+
+// Validation bei Click
+form.elements.human.addEventListener('click', function (event){
+	validateForm();
+});
+
+// Validation bei Submit
+$('#kontaktformular').submit(function (event) {
+	var data = {
+		'name': form.elements.name.value,
+		'email': form.elements.email.value,
+		'url': form.elements.url.value,
+		'category': form.elements.category.value,
+		'message': form.elements.message.value,
+		'copy': form.elements.copy.checked,
+		'human': form.elements.human.checked
+	};
+
+	// mit AJAX als POST zu mail.php schicken
+	$.ajax({
+		url: '/mail.php',
+		data: data,
+		type: 'POST',
+		success: function (data) {
+			// For Notification
+			const formStatus = document.getElementById("formStatus");
+			formStatus.show();
+			if(data.error){
+				formStatus.find('.icon').addClass('fa-exclamation-circle');
+				formStatus.find('.behind-icon').text(data.message);
+			}else{
+			    formStatus.find('.icon').addClass('fa-info-circle');
+			    formStatus.find('.behind-icon').text(data.message);
+			}
+		}
+	});
+	event.preventDefault();
+});
---
+++ mail.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * 
+ * E-Mail-Formular
+ * 
+ * **/
+
+// In der Produktion: Error-Reporting ausschalten
+//error_reporting(0);
+
+// Daten, die gesendet werden, sind in JSON-Format
+header('Content-Type: application/json');
+
+
+// if the url field is empty 
+if(isset($_POST['url']) && $_POST['url'] == ''){
+    // Variablen:
+    $name = @$_POST['name'];
+    $email = @$_POST['email'];
+    $url = @$_POST['url'];
+    $category = @$_POST['category'];
+    $message = @$_POST['message'];
+    $copy = @$_POST['copy'];
+    $human = @$_POST['human'];
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // E-Mail-Adresse nicht valide
+        echo json_encode(array(
+         'error'=> true,
+         'message' => 'E-Mail-Adresse nicht valide.'
+        ));
+    } elseif ($name == "") {
+        // Name nicht ausgefüllt
+        echo json_encode(array(
+         'error'=> true,
+         'message' => 'Namensfeld darf nicht leer sein.'
+        ));
+    } elseif ($message == "") {
+        // Nachricht ist leer
+        echo json_encode(array(
+         'error'=> true,
+         'message' => 'Nachricht darf nicht leer sein.'
+        ));
+    } elseif ($human == "false") {
+        // Human-Feld nicht checked
+        echo json_encode(array(
+         'error'=> true,
+         'message' => 'Diese Website nimmt nur Nachrichten von Menschen entgegen.'
+        ));
+    } else {
+        //Alles gut.
+
+        // E-Mails gehen an an:     
+        $youremail = 'mailformular@hutt.io';
+
+        // E-Mail zusammenbauen
+        $subject = '[' . $category . '] ' . 'Kontaktformular auf hutt.io';
+
+        // prepare a "pretty" version of the message
+        $body = "Nachricht von ". $name ." <". $email ."> über das Kontaktformular auf hutt.io:\n";
+        $body .= "$message";
+
+        // E-Mail-Adresse des Absenders verwenden, falls angegeben
+        if( $_POST['email'] && !preg_match( "/[\r\n]/", $_POST['email']) ) {
+            $headers = "From: " . $_POST['email'] . "\r\n";     
+        } else {
+            $headers = "From: " . $youremail . "\r\n"; 
+        }
+
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html\r\n";
+
+        $retval = mail($youremail, $subject, $body, $headers);
+
+        if($copy == "true") {
+            $headers = "From: " . $youremail . "\r\n";
+            $subject = '[' . $category . '] ' . 'Kontaktformular auf hutt.io [Kopie]';
+            $body = "Hallo ". $name .",\r\Du hast mir folgende Nachricht über das Kontaktformular auf hutt.io gesendet:\r\n" . $body;
+            mail($email, $subject, $body, $headers);
+        }
+
+        if ($retval == true) {
+          echo json_encode(array(
+             'success'=> true,
+             'message' => 'Deine Nachricht wurde erfolgreich versendet.'
+          ));
+        } else {
+          echo json_encode(array(
+             'error'=> true,
+             'message' => 'Deine Nachricht konnte leider nicht versendet werden.'
+          ));
+        }
+    }
+
+} else {
+
+    echo json_encode(array(
+     'error'=> true,
+     'message' => 'Ich nehme kein Spam an.'
+    ));
+
+}
